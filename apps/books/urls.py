7e705b2da24ff6f3d35dfdcf9a5469b1edfcb3d3
pai <paiuolo@gmail.com>
2016-03-03 18:02:57 +0100

    fix books urls

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -4,13 +4,13 @@ from .views import AuthorDetail, AuthorList, BookStoreDetail, BookStoreList, Ser
 
 urlpatterns = patterns('',
     url(r'^authors/$', AuthorList.as_view(), name='author_list'),
-    url(r'^authors/(?P<slug>[\w-]+)$', AuthorDetail.as_view(), name='author_detail'),
+    url(r'^authors/(?P<slug>[\w-]+)/$', AuthorDetail.as_view(), name='author_detail'),
     
     url(r'^bookstores/$', BookStoreList.as_view(), name='bookstore_list'),
     url(r'^bookstores/(?P<pk>\d+)/$', BookStoreDetail.as_view(), name='bookstore_detail'),
     
     url(r'^series/$', SerieList.as_view(), name='serie_list'),
-    url(r'^series/(?P<slug>[\w-]+)$', SerieDetail.as_view(), name='serie_detail'),
+    url(r'^series/(?P<slug>[\w-]+)/$', SerieDetail.as_view(), name='serie_detail'),
     
     #url(r'^digital_goods/(?P<pk>\d+)/', DigitalGoodDownload.as_view(), name='digital_good_download')
     
