174e9bdf1458e594c6b21ddb3ddaafe93709a4f6
pai <paiuolo@gmail.com>
2016-03-09 19:35:46 +0100

    added base deploy method

---
+++ deploy/deploy.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# check argomenti e set variabili
+# exit if no action specified
+ACTION=$1
+[ -z "$ACTION" ] && echo "Specificare Azione" && exit 1
+# set remote host
+HOST_REMOTO=$2
+[ -z "$HOST_REMOTO" ] && echo "Specificare url o IP dell'host" && exit 1
+# set remote user
+NOME_UTENTE=$3
+[ -z "$NOME_UTENTE" ] && echo "Specificare l'utente dell'host" && exit 1
+# set project name
+PROJECT_NAME=$4
+[ -z "$PROJECT_NAME" ] && echo "Specificare il nome del progetto" && exit 1
+
+
+REPO_NAME="django-oscar-bookstore"
+REPO_URL="https://github.com/paiuolo/${REPO_NAME}.git"
+
+PROJECT_DIR="/home/${NOME_UTENTE}/${PROJECT_NAME}/"
+REPOSITORY_DIR="${PROJECT_DIR}${REPO_NAME}/"
+
+DEPS="build-essential python3 python3-dev python3-venv libjpeg-dev libfreetype6-dev zlib1g-dev node-less redis-server git"
+
+printf "Password: "
+read -s USER_PASS
+
+# ACTIONS
+
+case "$ACTION" in
+"load")
+	fab -u $NOME_UTENTE -H $HOST_REMOTO install_deps:"$DEPS" -p $USER_PASS || exit 1
+	fab -u $NOME_UTENTE -H $HOST_REMOTO create_dir:$PROJECT_NAME -p $USER_PASS || exit 1
+	fab -u $NOME_UTENTE -H $HOST_REMOTO create_virtualenv:$PROJECT_NAME -p $USER_PASS || exit 1
+	fab -u $NOME_UTENTE -H $HOST_REMOTO install_python_deps:$PROJECT_NAME -p $USER_PASS || exit 1
+	fab -u $NOME_UTENTE -H $HOST_REMOTO clone_repo:$PROJECT_NAME,$REPO_URL -p $USER_PASS || exit 1
+	exit 0
+	;;
+"setup")
+    fab -u $NOME_UTENTE -H $HOST_REMOTO init_repo:$PROJECT_NAME,$REPO_NAME -p $USER_PASS || exit 1
+    exit 0
+    ;;
+"check")
+	fab -u $NOME_UTENTE -H $HOST_REMOTO lancia_test:$REPO_NAME -p $USER_PASS || exit 1
+	exit 0
+	;;
+*)
+    echo "Azione non prevista"
+	sleep 0
+	exit 1
+	;;
+esac
+
+exit 0
---
+++ deploy/fabfile.py
@@ -0,0 +1,50 @@
+from fabric.api import run, sudo, execute, settings
+
+def su(pwd, command):
+    with settings(
+        password= "%s" % pwd,
+        sudo_prefix="su - -c ",
+        sudo_prompt="Password:"
+        ):
+        sudo(command)
+
+def install_deps(packages):
+    run('su -c \'apt-get update && apt-get install '+packages+'\'')
+    #execute(su, pwd, 'apt-get update && apt-get install '+packages)
+
+def create_dir(project_name):
+    run('mkdir -p '+project_name)
+
+def create_virtualenv(project_name):
+    run('cd '+project_name+' && python3 -m venv virtualenv')
+
+def install_python_deps(project_name):
+    cmds = [
+        'cd '+project_name,
+        'source virtualenv/bin/activate',
+        'pip install git+https://github.com/paiuolo/django-oscar.git',
+        'pip install pycountry django-compressor django-debug-toolbar django-ckeditor redis',
+        'pip install -U celery[redis]',
+        'pip install git+https://github.com/paiuolo/django-oscar-paypal@modifiche_python3_utf'
+        ]
+    cmd = str.join(' && ', cmds)
+    run(cmd)
+    
+def clone_repo(project_name, repo_url):
+    run('cd '+project_name+' && git clone '+repo_url)
+    
+    
+def init_repo(project_name, repo_name):
+    cmds = [
+        'cd '+project_name,
+        'source virtualenv/bin/activate',
+        'cd '+repo_name,
+        'git checkout -b '+project_name,
+        './manage.py makemigrations',
+        './manage.py makemigrations paypal',
+        './manage.py migrate',
+        './manage.py oscar_populate_countries',
+        './manage.py loaddata bookstore/fixtures/initial_data.json',
+        ]
+    cmd = str.join(' && ', cmds)
+    run(cmd)
